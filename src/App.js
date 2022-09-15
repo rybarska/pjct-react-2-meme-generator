c5602c8afaad7366857f9fe0945aa2b31073324e
agata <rybarska>
2022-09-15 17:41:59 +0200

    Add inputs &  dropdown, construct new url from pieces

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+// import { load } from 'cheerio';
 
 function App() {
+  const [template, setTemplate] = useState('aag');
+
+  const [topText, setTopText] = useState('_');
+  const [bottomText, setBottomText] = useState('_');
+  // const [fullMemeUrl, setFullMemeUrl] = useState(
+  //   'https://api.memegen.link/images/aag.png',);
+  const fullMemeUrl =
+    'https://api.memegen.link/images/' +
+    template +
+    '/' +
+    topText +
+    '/' +
+    bottomText +
+    '.png';
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={fullMemeUrl}
+          className="App-memeImage"
+          alt="meme"
+          data-test-id="meme-image"
+        />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>the meme</code> and save to reload.
         </p>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={fullMemeUrl}
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <button>Download</button>
         </a>
       </header>
+      <label>
+        Top text
+        <br />
+        <br />
+        <input
+          // 2. Use state variable (connect it to the input)
+          value={topText} // {colorLuminosity}
+          // 3. Update the state variable when the user types something
+          onChange={(event) => {
+            setTopText(event.currentTarget.value);
+          }}
+        />
+      </label>
+      <br />
+      <br />
+      <label>
+        Bottom text
+        <br />
+        <br />
+        <input
+          // 2. Use state variable (connect it to the input)
+          value={bottomText} // {colorLuminosity}
+          // 3. Update the state variable when the user types something
+          onChange={(event) => {
+            setBottomText(event.currentTarget.value);
+          }}
+        />
+      </label>
+      <br />
+      <br />
+      <label htmlFor="templates">Meme template</label>
+      <br />
+      <br />
+      <select
+        id="templates"
+        name="templates"
+        value={template}
+        onChange={(event) => {
+          setTemplate(event.currentTarget.value);
+        }}
+      >
+        <option value="aag">Ancient Aliens Guy</option>
+        <option value="ackbar">It's A Trap!</option>
+        <option value="doge">doge</option>
+      </select>
+      <br />
+      <br />
     </div>
   );
 }
